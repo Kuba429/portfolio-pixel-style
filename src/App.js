c5471ef6eae49603d103cb7c137783044d3ffbd3
Kuba429 <jakubzawadzki429@gmail.com>
2021-05-15 22:26:13 +0200

    typo in App.js it's --> its

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LandingPage from './components/LandingPage'
 import GlobalStyle from './components/GlobalStyle'
 
 function App() {
-    console.log('Please ignore the error log. It has to do with glidejs library and i can\'t fix it without altering it\'s source.')
+    console.log('Please ignore the error log. It has to do with glidejs library and i can\'t fix it without altering its source.')
     return (
         <div className="App">
             <GlobalStyle/>
